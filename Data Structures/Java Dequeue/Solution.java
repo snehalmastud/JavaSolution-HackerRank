ae4cb0630a52bb038577b72679e2b399607d0dd7
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-22 20:35:40 +0530

    Data Structures Using Java!

---
+++ Data Structures/Java Dequeue/Solution.java
@@ -0,0 +1,36 @@
+ import java.util.*;
+    import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+    public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int input = in.nextInt();
+
+            deque.add(input);
+            set.add(input);
+
+            if (deque.size() == m) {
+                if (set.size() > max)
+                    max = set.size();
+
+                int first = (int) deque.remove();
+                if (!deque.contains(first))
+                    set.remove(first);
+            }
+        }
+        System.out.println(max);
+    }
+}
+
+
+
+
