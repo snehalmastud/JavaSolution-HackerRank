92f9f2daba87e7f9969b3d132ea46cce90d2bfef
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-22 19:41:57 +0530

    Introduction to Java!

---
+++ Introduction/Java If-Else/Solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 == 1) {
+            System.out.println("Weird");
+        } else if (n % 2 == 0 && n >= 2 && n <= 5) {
+            System.out.println("Not Weird");
+        } else if (n % 2 == 0 && n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+       }
+}
