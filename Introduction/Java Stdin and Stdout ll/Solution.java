967279b5a88388e3871f2308d8b6efebdcf9ba23
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-22 19:54:11 +0530

    Introduction to Java!

---
+++ Introduction/Java Stdin and Stdout ll/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        double f = sc.nextDouble();
+        sc.nextLine();
+        String line = sc.nextLine();
+
+        System.out.println("String: " + line);
+        System.out.println("Double: " + f);
+        System.out.println("Int: " +  x);
+    }
+}
+
