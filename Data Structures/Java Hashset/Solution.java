36f7df7fd76bec38710f761e4310a4df2580c4fb
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-22 20:31:43 +0530

    Data Structures Using Java!

---
+++ Data Structures/Java Hashset/Solution.java
@@ -0,0 +1,7 @@
+ s.close();
+
+        HashSet<String> set = new HashSet(t);
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(set.size());
+        }
